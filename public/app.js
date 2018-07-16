9629c463815cc0658e3a1737675023c4f7aa80a3
SIMOCCAuch19 <clerc6nathan@hotmail.fr>
2018-07-16 13:59:22 +0200

    authentification fonctionnelle

---
+++ public/app.js
@@ -0,0 +1,40 @@
+
+//affichage coté client du résultat de l'authentification
+function affiche (){
+
+	$.ajax({
+//Requête de type GET pour récupérer de la donnée envoyer depuis le serveur
+		type :'GET',
+//url a laquelle la donnée a été envoyer
+		url : '/res',
+//déclenchement de la fonction si la requête réussi
+		success : function(result){
+			alert(result);
+		}
+	});
+};
+
+
+
+//declenchement de la fonction au clique
+$('#valider').click(function(){
+
+//récupération des valeurs des inputs
+	var identifiant = $('#login').val();
+	var password = $('#mdp').val();
+
+	$.ajax({
+//Requête de type POST quand on envois de la donnée depuis le client
+		type:'POST',
+//l'url a laquelle on envois la donnée
+		url:'/auth',
+//donnée envoyer sous forme d'objet
+		data:{login:identifiant, mdp:password},
+//éxécution de la fonction affiche et vidage des inputs si la requête ajax réussi
+		success:function(){
+			affiche();
+			$('#login').val('');
+			$('#mdp').val('');
+		}
+	});
+});
---
+++ serveur.js
@@ -0,0 +1,62 @@
+//chargement des librairies js
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+//création de la variable result
+var result = '';
+
+//charge les fichiers static qui sont dans le dossier .../public
+//ces fichiers seront accessible a l'adresse .../static
+app.use('/static', express.static(__dirname + '/public'));
+
+//utilisation de body parser pour parsé les données envoyer
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//charge le fichier index.html(ici inutile car mon fichier est dans le dossier public, qui est charger juste au dessus)
+// app.get('/', function (req, res) {
+// 	res.sendfile('index.html');
+// });
+
+//définis le port ou il faut se connecté
+app.listen(3300, function () {
+	console.log('Connection sur le port 3300!');
+});
+
+//objet user (correspond a ma base de donné)
+var user ={
+	login: 'id',
+	mdp: 'mdp'
+}
+
+//fonction de comparaison
+app.post('/auth', function(req,res) {
+	var logs = [];
+//rq.body correspond a la requête recus en json et parser dans la foulé (par body parser)
+	compte=req.body;
+	logs.push(compte);
+
+	console.log(logs);
+	console.log(user.login);
+//comparaison des inputs avec la base de données
+	if (logs[0].login === user.login || logs[0].password === user.mdp) {
+
+		console.log('success');
+
+//résultat renvoyer si l'authentification est réussi
+		result = 'succes';
+	}else{
+
+		console.log('ça marche pas');
+
+//résultat renvoyer si l'authentification échoue
+		result = 'echec';
+	}
+	res.send();
+});
+
+//renvois au client le résultat de sa requête (en format json (res.json))
+app.get('/res', function(req, res) {
+ res.json(result);
+});
